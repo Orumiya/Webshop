41b05808d3bba1031ec7d126995daac4a69e9d14
Kasza Eszter <kasza.eszter.katalin@gmail.com>
2018-08-25 18:41:33 +0200

    2 buttons in the MainActivity

--- app/src/main/java/com/triptech/webshop/MainActivity.java
+++ app/src/main/java/com/triptech/webshop/MainActivity.java
@@ -1,7 +1,10 @@
 package com.triptech.webshop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button sign_in_button = findViewById(R.id.main_sign_in_button);
+        Button sign_up_button = findViewById(R.id.main_sign_up_button);
+        final Intent sign_in_intent = new Intent(this, LogInActivity.class);
+
+        sign_up_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(sign_in_intent);
+            }
+        });
+
+        final Intent sign_up_intent = new Intent(this, SignUpActivity.class);
+        sign_in_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(sign_up_intent);
+            }
+        });
+
     }
 }
